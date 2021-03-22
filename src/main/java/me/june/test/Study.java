bbe2fc68fc3c27f6581274e9ecfc6142f3441f7d
ncucu <ncucu.me@kakaocommerce.com>
2021-03-22 18:06:14 +0900

    Junit Assertions

--- src/main/java/me/june/test/Study.java
+++ src/main/java/me/june/test/Study.java
@@ -1,5 +1,26 @@
 package me.june.test;
 
 public class Study {
+	private Status status = Status.DRAFT;
+	private int limit;
 
+	public enum Status {
+		DRAFT,
+		;
+	}
+
+	public Study(int limit) {
+		if (limit > 0) {
+			throw new IllegalArgumentException("limit 은 0보다 커야합니다.");
+		}
+		this.limit = limit;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public int getLimit() {
+		return limit;
+	}
 }
--- src/test/java/me/june/test/StudyTest.java
+++ src/test/java/me/june/test/StudyTest.java
@@ -2,6 +2,8 @@ package me.june.test;
 
 import org.junit.jupiter.api.*;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -10,9 +12,34 @@ class StudyTest {
     @Test
 	@DisplayName("스터디 생성")
     void create() {
-        Study study = new Study();
+        Study study = new Study(1);
         assertNotNull(study);
-    }
+
+        // 람다식도 지원
+        assertEquals(Study.Status.DRAFT, study.getStatus(), "스터디를 처음 만들면 상태값이 DRAFT 여야 한다.");
+        assertEquals(Study.Status.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면 상태값이 DRAFT 여야 한다.");
+        assertTrue(study.getLimit() > 0, "스터디 참석 인원은 0 보다 커야합니다.");
+
+        // 각 테스트 들을 한번에 검증
+		assertAll(
+			() -> assertEquals(Study.Status.DRAFT, study.getStatus(), "스터디를 처음 만들면 상태값이 DRAFT 여야 한다."),
+			() -> assertEquals(Study.Status.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면 상태값이 DRAFT 여야 한다."),
+			() -> assertTrue(study.getLimit() > 0, "스터디 참석 인원은 0 보다 커야합니다.")
+		);
+
+		// 예외 검증
+		IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+		assertEquals(ex.getMessage(), "limit 은 0보다 커야합니다.");
+
+		// 실행 시간내에 완료되어야 하는지 검증
+		assertTimeout(Duration.ofSeconds(10), () -> new Study(10));
+
+		// 지정된 시간이 끝나면 테스트 실패로 간주하고 그냥 실패시키고 싶을경우
+		// 지정한 시간이 10초가 지나면 테스트는 실패할것이기 때문에 종료시킨다.
+		// 람다 코드블록 내에 존재하는 코드는 별도의 스레드에서 동작한다.
+		// 따라서 ThreadLocal 기반 기능 (Security, Transactional) 등이 예상과 다르게 동작할 수 있다.
+		assertTimeoutPreemptively(Duration.ofSeconds(10), () -> new Study(10));
+	}
 
     @Test
     @Disabled
